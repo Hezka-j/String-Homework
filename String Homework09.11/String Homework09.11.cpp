0e9137c19a3e33a19788b0da0e6b0dbdc006ab6d
Hezka-j <145163687+Hezka-j@users.noreply.github.com>
2023-11-10 00:27:18 +0200

    Add files via upload

---
+++ String Homework09.11/String Homework09.11.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int length = 50;
+    char* words = new char[length];
+    int count_words = 0;
+    int count_spaces = 0;
+    int count_symbols = 0;
+    int middle_length = 0;
+    cout << "Enter words - ";
+    gets_s(words, length);
+    for (int i = 0; i < strlen(words); i++)
+    {
+        if (words[i] == ' ')
+        {
+            count_spaces++;
+        }
+        cout << words[i] << " ";
+
+    }
+    cout << "\n";
+    //cout << words << "\n";
+    count_words = count_spaces + 1;
+    count_symbols = strlen(words) - count_spaces;
+    middle_length = count_symbols / count_words;
+    cout << "Middle length words - " << middle_length << "\n";
+    cout << "Count words - " << count_words << "\n";
+    cout << "Count spaces - " << count_spaces << "\n";
+    cout << "Count symbols(without spaces) - " << count_symbols << "\n";
+}
+
